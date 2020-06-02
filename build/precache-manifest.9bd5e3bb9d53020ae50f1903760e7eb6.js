d72ef513e6d34a977111d6dd77b405dd5e05f0c9
leevtori <vlee2@ualberta.ca>
2020-06-01 21:22:11 -0600

    cleaning up

---
+++ build/precache-manifest.9bd5e3bb9d53020ae50f1903760e7eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb81ddcecd47f82ca08d8ecf7c8048ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "244341b9ecfe75236a12",
+    "url": "/static/css/main.34cae7ce.chunk.css"
+  },
+  {
+    "revision": "61bb862ae21e1e345536",
+    "url": "/static/js/2.acf1030e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acf1030e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244341b9ecfe75236a12",
+    "url": "/static/js/main.af46e308.chunk.js"
+  },
+  {
+    "revision": "b2837a1e0d2f255f5667",
+    "url": "/static/js/runtime-main.819a195c.js"
+  }
+]);
